97c434d08a008a4f319237af320f0e7eaa6cb2fd
Vini220504 <150559952+Vini220504@users.noreply.github.com>
2023-11-11 19:53:52 -0300

    Update exercicio37.c

--- Lista Logica de Programacao/exercicio37.c
+++ Lista Logica de Programacao/exercicio37.c
@@ -3,7 +3,7 @@ dependem do tipo de praga e da área a ser contratada conforme a tabela:
 
 Tipo 1 – ervas daninhas R$ 50,00 por acre;
 Tipo 2 – gafanhotos R$ 100,00 por acre;
-Tipo 3 – broca R$ 150,00 por acre;
+Tipo 3 – broca R$ 50,00 por acre;
 Tipo 4 – todos acima R$ 250,00 por acre.
 
 Se a área a ser pulverizada for superior a 1000 acres, o fazendeiro tem um desconto de 5%.
